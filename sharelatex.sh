d83beae007180f54592815a4f9ea4c52677e8d15
Luca Zeug <luca@lucazeug.de>
2014-12-10 10:49:17 +0100

    better fix for mongodb-startup issue

--- sharelatex.sh
+++ sharelatex.sh
@@ -2,6 +2,12 @@
 export PATH=$PATH:/usr/local/texlive/2014/bin/x86_64-linux
 mongod &
 redis-server &
+
+# Waiting for mongodb to startup
+until nc -z localhost 27017
+do
+    sleep 1
+done
+
 cd /sharelatex
-sleep 10s
 grunt run
